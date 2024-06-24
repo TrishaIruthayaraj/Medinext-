69e0f53020a172d1695efebfe1da8708259f1fcf
TrishaIruthayaraj <131230270+TrishaIruthayaraj@users.noreply.github.com>
2024-06-24 09:41:02 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,14 @@
+// //console.log('name')
+// sun()
+// function sun(){
+// console.log('sun called')
+// }
+// const add=function(){
+//     console.log('add')
+// }
+const color=function(){
+    console.log('color')
+}
+const chocolateDescription = document.getElementById('root');
+chocolateDescription.style.backgroundColor='purple';
+alert('name');
---
+++ main.js
@@ -0,0 +1,23 @@
+const btn = document.getElementById('alertBtn');
+btn.addEventListener('click', () => {
+    alert('Hi!');
+});
+
+const pop = document.getElementById('Btn');
+pop.addEventListener('click', () => {
+    alert('Hi!');
+});
+
+const active = document.getElementById('active');
+active.addEventListener('click', () => {
+    alert('Home');
+});
+const Top = document.getElementById('Top');
+Top.addEventListener('click', () => {
+    alert('About Us');
+});
+const Class = document.getElementById('Class');
+Class.addEventListener('click', () => {
+    alert('Class');
+});
+
